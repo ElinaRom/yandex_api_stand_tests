bcf21c72369ea80535b1bb098595074b81fa4491
RomantsovaEI <elinaromantsova@gmail.com>
2024-02-18 14:45:03 +0700

    My first commit

---
+++ configuration.py
@@ -0,0 +1,7 @@
+URL_SERVICE = "https://b48fa9fd-f9e4-4245-ba9f-146e20de6053.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
